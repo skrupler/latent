0d3aeb258ae56061f960ef0039850f063ba14573
skrupler <61eefedcb8c1e879@protonmail.ch>
2017-02-02 14:26:28 +0100

    restruct

--- latent.sh
+++ latent.sh
@@ -1,14 +1,7 @@
 #!/bin/bash   
 
-### BEGIN INIT INFO
-# Provides:          rtorrent 
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Start daemon at boot time
-# Description:       Enable service provided by daemon.
-### END INIT INFO
+source helpers/functions.sh
+
 
 # Settings
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
